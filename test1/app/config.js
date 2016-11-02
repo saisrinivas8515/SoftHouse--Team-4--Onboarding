425534f190dff1c6b6b8b6c0006625666d43e670
shivacherla <shivacherla93@gmail.com>
2016-11-02 15:31:35 +0100

    refreshing master

--- test1/app/config.js
+++
@@ -1,8 +0,0 @@
-function appConfig($routeProvider) {
-    $routeProvider
-        .when('/', { template: '<products></products>' })
-        .when('/info', { template: '<info></info>' })
-        .otherwise({
-            redirectTo: '/'
-        })
-}
--- test1/app/info/info.tpl
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <navigation class="row"></navigation>
-
-    <div class="row">
-        <div class="col-xs-12">This is the home page. Use the navigation to manage products.</div>
-    </div>
-
-</div>
-
