4ea9c43e83883a0355828c8e2bffc3e3d9e6b169
shivacherla <shivacherla93@gmail.com>
2016-11-30 19:49:51 +0100

    accept,reject,pending status icons

--- backend/src/main/java/team4/softhouse/db/entity/Inventory.java
+++ backend/src/main/java/team4/softhouse/db/entity/Inventory.java
@@ -11,7 +11,7 @@ public class Inventory {
 
     @JsonProperty
     @NotEmpty
-    @Length(max =10 , message = "must not be longer than 10 characters")
+    @Length(max =50 , message = "must not be longer than 50 characters")
     private String name;
 
     @JsonProperty
